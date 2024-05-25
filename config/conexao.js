055d1cf1e04f983be74242dbe67e52436b4d1d83
rosadavi <davi.rosa.ofc@gmail.com>
2024-05-25 13:31:35 -0300

    teste

--- config/conexao.js
+++ config/conexao.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('wanderso_davirosa', 'wanderso_davirosa_user', 'KmRO4nIxYEuo', {
     host: '209.133.214.194',
-    dialect: 'mysql',
+    dialect: 'mysql2',
 });
 
 module.exports = {
