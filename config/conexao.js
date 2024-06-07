7d6ddfe87413c66cc440b422a54942a4c6b68e34
rosadavi <davi.rosa.ofc@gmail.com>
2024-06-07 12:20:57 -0300

    atualizando conexao bd

--- config/conexao.js
+++ config/conexao.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('wanderso_davirosa', 'wanderso_davirosa_user', 'KmRO4nIxYEuo', {
-    host: '209.133.214.194',
+    host: '199.193.117.238',
     dialect: 'mysql',
 });
 
